e9c22ef93d1c56b6429f2c8c928fdb476075503f
fatumayussuf <fatumamaax2@gmail.com>
2024-11-14 10:47:19 +0300

    Initial commit from create-remix

---
+++ app/routes/_index.jsx
@@ -0,0 +1,12 @@
+import { Children, useState } from "react";
+
+export const meta = () => {
+  return [
+    { title: "Tip calculator" },
+    { name: "description", content: "Calculate tips easily!" },
+  ];
+};
+
+export default function Index() {
+  return
+}
